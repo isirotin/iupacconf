b35fb067333b82c5a2c03dd0cec46d46578a3a7c
chem1st <chem1st@devMachine.(none)>
2016-08-18 18:30:00 +0400

    1st migration made & superuser added

--- iupac/config/urls.py
+++ iupac/config/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from main.views import HomeView
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
