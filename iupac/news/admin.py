ab360170c77db783e77b32b6c10e8ac0e13f64e4
Igor S. Sirotin <isirotin@muctr.ru>
2016-08-28 13:24:30 +0300

    Added news models.

--- iupac/news/admin.py
+++ iupac/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import *
+
+
+class NewsItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(NewsItem, NewsItemAdmin)
+
--- iupac/news/models.py
+++ iupac/news/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from taggit.managers import TaggableManager
+from content.models import HTMLContent
+from django.utils import timezone
+from event.models import Event
 
-# Create your models here.
+
+class NewsItem(models.Model):
+    title = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255, blank=True, null=True)
+    content = models.ForeignKey(HTMLContent, blank=True, null=True)
+    slug = models.SlugField()
+    tags = TaggableManager()
+    important = models.BooleanField(default=False)
+    show_datetime = models.DateTimeField(default=timezone.now)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    related_event = models.ForeignKey(Event, blank=True, null=True)
+
+    def get_absolute_url(self):
+        url = "/news/%s/" % self.slug
+        return url
+
+    def __unicode__(self):
+        return self.title
